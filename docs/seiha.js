38779eccae5be3162719e97b3b6da32677307d27
cicada_sss <sss.cicada@gmail.com>
2017-10-09 11:53:35 +0900

    add

---
+++ docs/seiha.js
@@ -0,0 +1,24 @@
+var data = "";
+
+setInterval(function() {
+    var a = Math.floor(Math.random() * 51);
+    console.log(a);
+    if (a == 0) {
+        data = "あ<br>" + data;
+    }
+    $("#timeline").html(data);
+    $("#timeline").show();
+    if ( data.indexOf('雲<br>鯖<br>完<br>全<br>制<br>覇<br>') != -1) {
+alert("おめでとう");
+data = "";
+}
+}, 100);
+
+function toot1() {
+    var val = $('#toot [name=toot]').val();
+    console.log(val);
+    data = val + "<br>" + data;
+    $("#timeline").html(data);
+    $("#timeline").show();
+    $('#toot [name=toot]').val("");
+}
